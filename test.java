35ab87bcb4240f5372e742a38ced47a8aa3da1f8
Parsh-Jain <parsh23368@iiitd.ac.in>
2024-10-17 23:52:30 +0530

    Initializing

--- test.java
+++ test.java
@@ -1,7 +1,4 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("ggg");
-        System.out.println("fdsfsdff");
-        System.out.println("hithtrbtgb ");
     }
 }
