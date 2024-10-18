5befe5c47bc8ac8e39e372607771dab01cac8eb7
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-18 23:52:45 +0530

    homescreen added

---
+++ core/src/main/java/com/ap/angrybirds/HomeScreen.java
@@ -0,0 +1,23 @@
+package com.ap.angrybirds;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class HomeScreen extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture background;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("homeScreen background.jpg");
+    }
+}
