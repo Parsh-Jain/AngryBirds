eef505c68432a602dfa67fccaff777e5c9cac101
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-22 22:38:34 +0530

    Some images are added

---
+++ core/src/main/java/com/ap/angrybirds/MafiaPig.java
@@ -0,0 +1,9 @@
+package com.ap.angrybirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class MafiaPig extends Pigs{
+    public MafiaPig(Texture pigTexture) {
+        super(new Texture("MafiaPigs.jpg"));
+    }
+}
