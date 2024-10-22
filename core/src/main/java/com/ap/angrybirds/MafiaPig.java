9c56536d448a64676375f2a8f8a6aedc2d16bf4c
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-23 00:52:46 +0530

    Level Screen Updation

--- core/src/main/java/com/ap/angrybirds/MafiaPig.java
+++ core/src/main/java/com/ap/angrybirds/MafiaPig.java
@@ -5,5 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 public class MafiaPig extends Pigs{
     public MafiaPig(Texture pigTexture) {
         super(new Texture("MafiaPig.png"));
+        setSize(75,75);
     }
 }
