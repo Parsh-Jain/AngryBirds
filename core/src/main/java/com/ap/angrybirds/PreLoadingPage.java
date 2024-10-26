09c20bc116e62d2d6f767dab9948c6130ed901e9
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-26 20:31:49 +0530

    PreLoadingPage code is refined

--- core/src/main/java/com/ap/angrybirds/PreLoadingPage.java
+++ core/src/main/java/com/ap/angrybirds/PreLoadingPage.java
@@ -14,21 +14,17 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PreLoadingPage extends ScreenAdapter {
-
     Main main;
     Sprite sprite;
     Texture image;
     Batch batch;
     OrthographicCamera camera;
     Viewport viewport;
-
-    // Flicker and timer variables
     float worldWidth = 1920;
     float worldHeight = 1080;
     float flickerTimer = 0f;
-    boolean showPressEnter = false; // Toggle for flickering
-
-    Music enterSound; // Sound for Enter key
+    boolean showPressEnter = false;
+    Music enterSound;
 
     public PreLoadingPage(Main main) {
         this.main = main;
@@ -36,7 +32,6 @@ public class PreLoadingPage extends ScreenAdapter {
 
     @Override
     public void show() {
-        // Initialize camera, viewport, and background image
         camera = new OrthographicCamera();
         viewport = new FitViewport(worldWidth, worldHeight, camera);
         viewport.apply();
@@ -47,51 +42,37 @@ public class PreLoadingPage extends ScreenAdapter {
         image = new Texture("loadingpage.jpg");
         sprite = new Sprite(image);
         sprite.setPosition(worldWidth / 2 - sprite.getWidth() / 2, worldHeight / 2 - sprite.getHeight() / 2);
-
-        // Load sound
         enterSound = Gdx.audio.newMusic(Gdx.files.internal("EnterSound.mp3"));
     }
 
     @Override
     public void render(float delta) {
-        // Clear screen
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Draw background
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         sprite.draw(batch);
         batch.end();
-
-        // Flicker effect for "Please press Enter to start!" message
         flickerTimer += delta;
-        if (flickerTimer > 0.5f) { // Flicker every half second
+        if (flickerTimer > 0.5f) {
             showPressEnter = !showPressEnter;
             flickerTimer = 0;
         }
-
-        // Draw "Please press Enter" message if toggle is on
         if (showPressEnter) {
             batch.begin();
             main.font.getData().setScale(2f);
             main.font.draw(batch, "Please press Enter to start!", worldWidth / 2 - 200, 100);
-//            main.font.getData().setScale(1.0f);
             batch.end();
         }
-
-        // Check for Enter key press to play sound and switch screen
         if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
             enterSound.play();
-            main.setScreen(new LoadingPage(main)); // Change to next screen
+            main.setScreen(new LoadingPage(main));
         }
     }
-
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
     }
-
     @Override
     public void dispose() {
         batch.dispose();
