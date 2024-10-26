28ac222fb3d4ba0065a735517007c66537fa4dfb
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-26 20:34:07 +0530

    LoadingPage code is refined

--- core/src/main/java/com/ap/angrybirds/LoadingPage.java
+++ core/src/main/java/com/ap/angrybirds/LoadingPage.java
@@ -11,9 +11,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-
 public class LoadingPage extends ScreenAdapter {
-
     Main main;
     ShapeRenderer shapeRenderer;
     Sprite sprite;
@@ -21,35 +19,27 @@ public class LoadingPage extends ScreenAdapter {
     Batch batch;
     OrthographicCamera camera;
     Viewport viewport;
-
     float worldWidth = 1920;
     float worldHeight = 1080;
-    float lineWidth = 0; // Initial width of the loading line
-    float maxLineWidth; // Maximum line width (screen width)
-    float loadingPercentage = 0; // Starting percentage
+    float lineWidth = 0;
+    float maxLineWidth;
+    float loadingPercentage = 0;
     float timer;
-
     public LoadingPage(Main main) {
         this.main = main;
     }
-
     @Override
     public void show() {
-        // Set up the camera and viewport
         camera = new OrthographicCamera();
         viewport = new FitViewport(worldWidth, worldHeight, camera);
         viewport.apply();
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         camera.update();
-
-        // Initialize renderers and textures
         shapeRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
         image = new Texture("loadingpage.jpg");
         sprite = new Sprite(image);
         sprite.setPosition(worldWidth / 2 - sprite.getWidth() / 2, worldHeight / 2 - sprite.getHeight() / 2);
-
-        // Set maximum line width based on screen width
         maxLineWidth = worldWidth;
         timer = 0;
     }
@@ -58,41 +48,29 @@ public class LoadingPage extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Draw background image
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         sprite.draw(batch);
         batch.end();
-
-        // Update line width and loading percentage
-        lineWidth += (maxLineWidth / 4) * delta; // Slower speed for loading line
-        loadingPercentage = Math.min((lineWidth / maxLineWidth) * 100, 100); // Percentage
-
-        // Draw the loading line at a lower y-axis position
+        lineWidth += (maxLineWidth / 4) * delta;
+        loadingPercentage = Math.min((lineWidth / maxLineWidth) * 100, 100);
         shapeRenderer.setProjectionMatrix(camera.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(1, 1, 1, 1);
-        shapeRenderer.rect(0, 100, lineWidth, 20); // Move line lower by adjusting y position
+        shapeRenderer.rect(0, 100, lineWidth, 20);
         shapeRenderer.end();
-
-        // Draw the percentage text
         batch.begin();
-        main.font.draw(batch, (int) loadingPercentage + "%", worldWidth / 2 - 25, 150); // Adjusted text position for new line height
+        main.font.draw(batch, (int) loadingPercentage + "%", worldWidth / 2 - 25, 150);
         batch.end();
-
-        // Check if loading is complete
         if (loadingPercentage >= 100) {
-            main.setScreen(new HomeScreen(main));  // Move to the next screen
+            main.setScreen(new HomeScreen(main));
         }
     }
-
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
     }
-
     @Override
     public void dispose() {
         shapeRenderer.dispose();
