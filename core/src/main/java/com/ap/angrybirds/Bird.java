6f84091c4a241e9de7ae7b612b41b3501c1faaa7
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-27 21:52:08 +0530

    Comments are added in Bird.java

--- core/src/main/java/com/ap/angrybirds/Bird.java
+++ core/src/main/java/com/ap/angrybirds/Bird.java
@@ -5,25 +5,24 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Bird extends Actor {
-    protected Texture birdTexture;
+    protected Texture birdTexture; // Attribute
 
-    public Bird(Texture birdTexture) {
+    public Bird(Texture birdTexture) { // Constructor
         this.birdTexture = birdTexture;
         setSize(100, 100);  // Default size
     }
 
     @Override
-    public void draw(Batch batch, float parentAlpha) {
+    public void draw(Batch batch, float parentAlpha) { //Drawing Bird Texture
         batch.draw(birdTexture, getX(), getY(), getWidth(), getHeight());
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        // Bird-specific actions here (e.g., flying)
     }
 
-    public void dispose() {
+    public void dispose() { // Disposing Bird Texture
         birdTexture.dispose();
     }
 }
