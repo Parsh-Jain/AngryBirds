4a1317bf4b76fc214197ec92b1caee567f115229
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-27 22:29:15 +0530

    Comments are added in SuccessfulEndScreen.java

--- core/src/main/java/com/ap/angrybirds/SuccessfulEndScreen.java
+++ core/src/main/java/com/ap/angrybirds/SuccessfulEndScreen.java
@@ -1,6 +1,5 @@
 package com.ap.angrybirds;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Music;
@@ -12,37 +11,29 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-
-
 public class SuccessfulEndScreen extends ScreenAdapter {
-    Main main;
+    Main main; // Important Attributes
     SpriteBatch batch;
     Texture background;
     Texture levelMenu;
     Texture text;
     Rectangle levelMenu_Button;
     Music LevelButtonSound;
-
     OrthographicCamera camera;
     Viewport viewport;
-
     float worldWidth = 1920;
     float worldHeight = 1080;
-
     public SuccessfulEndScreen(Main main){
         this.main = main;
-    }
-
-
+    } // Constructor
     @Override
-    public void show(){
+    public void show(){ // show method to create all the attributes
         batch = new SpriteBatch();
         background = new Texture("SuccessfullBackground.png");
         text = new Texture("Level Menu.png");
         levelMenu = new Texture("BasicButton.png");
         levelMenu_Button = new Rectangle(690,45,520,122);
         LevelButtonSound=Gdx.audio.newMusic(Gdx.files.internal("NormalButtonSound.mp3"));
-
         camera = new OrthographicCamera();
         viewport = new FitViewport(worldWidth, worldHeight, camera);
         viewport.apply();
@@ -51,27 +42,23 @@ public class SuccessfulEndScreen extends ScreenAdapter {
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float delta) { // Rendering
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         camera.update();
         batch.setProjectionMatrix(camera.combined);
-
         batch.begin();
         batch.draw(background, 0, 0);
-        //batch.draw(levelSelectionMenu, 755, 50 );
         batch.draw(levelMenu,690,45,520,122);
         batch.draw(text, 755, 65,400,80);
         batch.end();
-
-
+        // Checking User Input
         if(Gdx.input.isTouched()){
             Vector2 touchPos=new Vector2(Gdx.input.getX(),Gdx.input.getY());
             viewport.unproject(touchPos);
             if(levelMenu_Button.contains(touchPos.x,touchPos.y)){
                 LevelButtonSound.play();
-                main.setScreen(new LevelPage(main));
+                main.setScreen(new LevelPage(main)); // switch to Levelpage
             }
         }
     }
@@ -79,10 +66,10 @@ public class SuccessfulEndScreen extends ScreenAdapter {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
-    }
+    } // Resizing
 
     @Override
     public void dispose() {
         super.dispose();
-    }
+    } // Disposing
 }
