4a36e0e13e6f081dc8aa5d177db37af543e9d298
Parsh-Jain <parsh23368@iiitd.ac.in>
2024-10-27 15:34:07 +0530

    working on settings page

--- core/src/main/java/com/ap/angrybirds/Settings.java
+++ core/src/main/java/com/ap/angrybirds/Settings.java
@@ -24,12 +24,15 @@ public class Settings extends ScreenAdapter {
     private Texture music_word;
     private Texture BackToHomePage;
     private Texture BackToLevelMenu;
+    private Texture info;
+    private Texture infoText;
     private Texture blueBox;
-    //private Texture blueBox1;
+
 
     private OrthographicCamera camera;
     private Viewport viewport;
     private SpriteBatch batch;
+    Rectangle BackToHomepageButton, BackToLevelMenuButton;
 
     public Settings(Main main) {
         this.main = main;
@@ -49,11 +52,15 @@ public class Settings extends ScreenAdapter {
         settingsBackground = new Texture("settingBox.png");
         sound = new Texture("sound.png");
         music = new Texture("music.png");
+        info = new Texture("info.png");
         sound_word = new Texture("soundWord.png");
         music_word = new Texture("musicWord.png");
+        infoText = new Texture("infoText.png");
         BackToHomePage = new Texture("BackToHomePage.png");
         BackToLevelMenu = new Texture("BackToLevelMenu.png");
         blueBox = new Texture("blueBox.png");
+        BackToHomepageButton = new Rectangle(740, 436, 500, 100);
+        BackToLevelMenuButton = new Rectangle(740, 310, 500, 100);
     }
 
     public void render(float delta){
@@ -65,18 +72,29 @@ public class Settings extends ScreenAdapter {
         batch.begin();
         batch.draw(Background, 0, 0);
         batch.draw(settingsBackground, 400, 80, 1200,1000);
-        batch.draw(music, 680, 660, 125,125);
-        batch.draw(sound, 870, 660, 125,125);
-        batch.draw(sound_word, 870, 620, 127,33);
-        batch.draw(music_word, 680, 620, 127,33);
-        batch.draw(blueBox, 810, 436, 385, 100);
-        batch.draw(blueBox, 810, 310, 385, 100);
-        batch.draw(BackToHomePage, 839, 450, 320,65);
-        batch.draw(BackToLevelMenu, 830, 320, 340,80);
-
+        batch.draw(music, 710, 660, 125,125);
+        batch.draw(sound, 920, 660, 125,125);
+        batch.draw(info, 1130, 660, 140,125);
+        batch.draw(music_word, 710, 620, 127,33);
+        batch.draw(sound_word, 920, 620, 127,33);
+        batch.draw(infoText, 1130, 613, 127,40);
+        batch.draw(blueBox, 740, 436, 500, 100);
+        batch.draw(blueBox, 740, 310, 500, 100);
+        batch.draw(BackToHomePage, 715, 450, 550,85);
+        batch.draw(BackToLevelMenu, 685, 320, 580,80);
         batch.end();
 
-        System.out.println("X: " + Gdx.input.getX() + " Y: " + Gdx.input.getY());
+        if(Gdx.input.isTouched()) {
+            Vector2 touchPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            if (BackToHomepageButton.contains(touchPos.x, touchPos.y)) {
+                main.setScreen(new HomeScreen(main));
+            }else if (BackToLevelMenuButton.contains(touchPos.x, touchPos.y)) {
+                main.setScreen(new LevelPage(main));
+            }
+        }
+
+
     }
 
     @Override
