4b2bc9704541742cc3d5bbaf2b049adb8e03ee3b
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-27 22:31:38 +0530

    Comments are added in YellowBird.java

--- core/src/main/java/com/ap/angrybirds/YellowBird.java
+++ core/src/main/java/com/ap/angrybirds/YellowBird.java
@@ -3,8 +3,8 @@ package com.ap.angrybirds;
 import com.badlogic.gdx.graphics.Texture;
 
 public class YellowBird extends Bird{
-    public YellowBird(Texture birdTexture) {
-        super(new Texture("YellowAngryBird.png"));
+    public YellowBird(Texture birdTexture) { // Constructor
+        super(new Texture("YellowAngryBird.png")); // Accessing Constructor of Bird
         setSize(80,80);
     }
     @Override
