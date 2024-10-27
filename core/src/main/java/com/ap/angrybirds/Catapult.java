513b89a0051cd968c02f5ce813c0a8b8b7bc89da
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-27 21:56:21 +0530

    Comments are added in Catapult.java

--- core/src/main/java/com/ap/angrybirds/Catapult.java
+++ core/src/main/java/com/ap/angrybirds/Catapult.java
@@ -5,26 +5,25 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Catapult extends Actor {
-    protected Texture CatapultTexture;
+    protected Texture CatapultTexture; // Attribute
 
-    public Catapult(Texture CatapultTexture) {
+    public Catapult(Texture CatapultTexture) { // Constructor
         this.CatapultTexture = CatapultTexture;
-        setSize(450, 300);  // Default size
+        setSize(450, 300);
     }
 
     @Override
-    public void draw(Batch batch, float parentAlpha) {
+    public void draw(Batch batch, float parentAlpha) { // Drawing Catapult Texture
         batch.draw(CatapultTexture, getX(), getY(), getWidth(), getHeight());
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        // Pig-specific actions here
     }
 
     public void dispose() {
         CatapultTexture.dispose();
-    }
+    } // Disposing Catapult Texture
 }
 
