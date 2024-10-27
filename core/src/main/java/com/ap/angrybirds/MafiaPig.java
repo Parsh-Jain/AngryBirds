8b8cead511f069878836b0ab2fb6b522871fb636
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-27 22:16:49 +0530

    Comments are added in MafiaPig.java

--- core/src/main/java/com/ap/angrybirds/MafiaPig.java
+++ core/src/main/java/com/ap/angrybirds/MafiaPig.java
@@ -3,8 +3,8 @@ package com.ap.angrybirds;
 import com.badlogic.gdx.graphics.Texture;
 
 public class MafiaPig extends Pigs{
-    public MafiaPig(Texture pigTexture) {
-        super(new Texture("MafiaPig.png"));
+    public MafiaPig(Texture pigTexture) { // Constructor
+        super(new Texture("MafiaPig.png"));  // Accessing Constructor of Pigs`
         setSize(75,75);
     }
 }
