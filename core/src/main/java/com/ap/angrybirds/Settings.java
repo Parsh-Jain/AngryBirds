a9ebd9b9262043872a6b61d373723293b058c185
karaniniiitd <karan23271@iiitd.ac.in>
2024-10-27 22:26:35 +0530

    Comments are added in Setting.java

--- core/src/main/java/com/ap/angrybirds/Settings.java
+++ core/src/main/java/com/ap/angrybirds/Settings.java
@@ -16,7 +16,7 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Settings extends ScreenAdapter {
-    Main main;
+    Main main;  // Important Attributes
     private Texture Background;
     private Texture settingsBackground;
     private Texture sound;
@@ -29,27 +29,20 @@ public class Settings extends ScreenAdapter {
     private Texture infoText;
     private Texture blueBox;
     private Music EnterSound;
-
-
     private OrthographicCamera camera;
     private Viewport viewport;
     private SpriteBatch batch;
     Rectangle BackToHomepageButton, BackToLevelMenuButton;
-
     public Settings(Main main) {
         this.main = main;
-    }
-
-
-    public void show(){
+    } // Constructor
+    public void show(){ // Show method to create all the attributes
         camera = new OrthographicCamera();
         viewport = new FitViewport(1920, 1080, camera);
         viewport.apply();
         camera.position.set(1920 / 2, 1080 / 2, 0);
         camera.update();
-
-        batch = new SpriteBatch();  // Initialize the batch
-
+        batch = new SpriteBatch();
         Background = new Texture("settingsBackground.jpg");
         settingsBackground = new Texture("settingBox.png");
         sound = new Texture("sound.png");
@@ -66,12 +59,10 @@ public class Settings extends ScreenAdapter {
         BackToLevelMenuButton = new Rectangle(740, 310, 500, 100);
     }
 
-    public void render(float delta){
+    public void render(float delta){ // Rendering
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
-        //
-
         batch.begin();
         batch.draw(Background, 0, 0);
         batch.draw(settingsBackground, 400, 80, 1200,1000);
@@ -86,16 +77,16 @@ public class Settings extends ScreenAdapter {
         batch.draw(BackToHomePage, 715, 450, 550,85);
         batch.draw(BackToLevelMenu, 685, 320, 580,80);
         batch.end();
-
+        //Checking User Input
         if(Gdx.input.isTouched()) {
             Vector2 touchPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
             viewport.unproject(touchPos);
             if (BackToHomepageButton.contains(touchPos.x, touchPos.y)) {
                 EnterSound.play();
-                main.setScreen(new HomeScreen(main));
+                main.setScreen(new HomeScreen(main)); // switch to Homescreen
             }else if (BackToLevelMenuButton.contains(touchPos.x, touchPos.y)) {
                 EnterSound.play();
-                main.setScreen(new LevelPage(main));
+                main.setScreen(new LevelPage(main)); // switch ot Levelpage
             }
         }
 
@@ -105,9 +96,9 @@ public class Settings extends ScreenAdapter {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
-    }
+    } //Resizing
 
-    public void dispose(){
+    public void dispose(){ // Disposing
         batch.dispose();
         EnterSound.dispose();
         Background.dispose();
